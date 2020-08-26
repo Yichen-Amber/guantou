26cc8faba4fc405d19424166501cd7266fb9284c
Yichen <2283815345@qq.com>
2020-08-27 06:00:34 +0800

    html和css写好了

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,12 @@
 <style lang="scss">
   @import "~@/assets/style/helper.scss";
   @import "~@/assets/style/reset.scss";
-
   body {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #333;
     font-family: $font-hei;
+    font-size: 16px;
     line-height: 1.5;
   }
 </style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`   ">
       <slot/>
     </div>
     <Nav/>
   </div>
 </template>
+
 <script lang="ts">
   export default {
+    props: ['classPrefix'],
     name: 'Layout'
   };
 </script>
 
 <style scoped lang="scss">
-  .nav-wrapper {
+  .layout-wrapper {
     border: 1px solid green;
     display: flex;
     flex-direction: column;
