87b98b2100bc73862183ab7eb3048547301ab4d0
Yichen <2283815345@qq.com>
2020-09-07 10:45:35 +0800

    将create封装到recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -41,14 +41,11 @@
       this.record.notes = value;
     }
     saveRecord() {
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
-      console.log(this.recordList);
+      recordListModel.create(this.record);
     }
     @Watch('recordList')
     onRecordListChange() {
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
   }
 </script>
