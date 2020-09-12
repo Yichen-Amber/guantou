7fee08cc9b9d107722b87fc60132860d5d7c14a1
Yichen <2283815345@qq.com>
2020-09-12 11:36:23 +0800

    抽离Tabs.vua，使用Statistics.vua里面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,28 +1,49 @@
 <template>
-  <Layout class="nav-wrapper">
-    statistics.vue
-
+  <Layout>
+    <Tabs class-prefix="types" :data-source="typeList" :value.sync="type"/>
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+    <div>
+      type:{{type}}
+      <br/>
+      interval:{{interval}}
+    </div>
   </Layout>
 </template>
 
-<script lang="ts">
-
-  export default {
-    name: 'Statistics'
-  };
-</script>
-
 <style scoped lang="scss">
-  .nav-wrapper {
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
+  ::v-deep .types-tabs-item {
+    background: white;
 
-  .content {
-    border: 1px solid blue;
-    overflow: auto;
-    flex-grow: 1;
+    &.selected {
+      background: #c4c4c4;
+
+      &::after {
+        display: none;
+      }
+    }
   }
 </style>
+<script lang="ts">
+
+  import Types from '@/components/Money/Types.vue';
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+
+  @Component({
+    components: {Tabs, Types},
+  })
+  export default class Statistics extends Vue {
+    type = '-';
+    interval = 'day';
+    intervalList = [
+      {text: '按天', value: 'day'},
+      {text: '按周', value: 'week'},
+      {text: '按月', value: 'month'},
+    ];
+    typeList = [
+      {text: '支出', value: '-'},
+      {text: '收入', value: '+'},
+    ];
+  }
+</script>
